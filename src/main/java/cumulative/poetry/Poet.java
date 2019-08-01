b698d435b7cd4b44848fc031b6ea1d764991b8cb
Hariharan Sivakumar <hariharans@INDCHN-Hariharans-MacBook-Pro.local>
2019-08-01 16:59:58 +0530

    Update for Relase2

--- src/main/java/cumulative/poetry/Poet.java
+++ src/main/java/cumulative/poetry/Poet.java
@@ -1,19 +1,30 @@
 package cumulative.poetry;
 
+import java.util.Arrays;
+
 public class Poet {
     public static final String[] tot = new String[]{"house that Jack built", "malth that lay in", "rat that ate", "cat that killed", "dog that worried", "cow with the crumpled horn that tossed", "maiden all forlorn that milked", "man all tattered and torn that kissed", "priest all shaven and shorn that married", "rooster that crowed in the morn that woke", "farmer sowing his corn that kept", "horse and the hound and the horn that belonged to"};
 
     public Poet() {
     }
 
-    public String recite(int tillday) {
-        StringBuilder s = new StringBuilder();
-        String var10001 = tot[tillday - 1];
-        s.append("This is the " + var10001 + "\n");
 
+    public String recite(int tillday,boolean cond)
+    {
+        StringBuilder s = new StringBuilder();
+        s.append("This is the " + tot[tillday-1] + "\n");
+
+        if(cond)
+        {
+            s.append(" the "+tot[tillday-1]+"\n");
+        }
         for(int i = tillday - 2; i >= 0; --i) {
-            var10001 = tot[i];
-            s.append(" the " + var10001 + "\n");
+            String add = " the " + tot[i] + "\n";
+            s.append(add);
+            if(cond)
+            {
+                s.append(add);
+            }
         }
 
         return s.toString();
@@ -21,12 +32,17 @@ public class Poet {
 
     public static void main(String[] args) {
         Poet p = new Poet();
-        if (args.length == 2) {
-            System.out.println(p.recite(Integer.parseInt(args[1])));
-        } else {
+        boolean echo = Arrays.asList(args).contains("--echo");
+        boolean reveal_for_day = Arrays.asList(args).contains("--reveal-for-day");
+        boolean recite = Arrays.asList(args).contains("--recite");
+        System.out.println(echo);
+        if (args.length >= 2 && reveal_for_day) {
+            System.out.println(p.recite(Integer.parseInt(args[1]),echo));
+        } else if(args.length<=2 && recite)
+        {
             for(int i = 1; i < tot.length; ++i) {
                 System.out.println("Day " + i);
-                System.out.println(p.recite(i));
+                System.out.println(p.recite(i,echo));
             }
         }
 
